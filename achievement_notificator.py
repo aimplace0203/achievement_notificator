0440c4705976a9bc734b281ae0d7ee8ef3a81dda
kefujiwa <kefujiwa@student.42tokyo.jp>
2022-03-11 15:49:11 +0900

    fix: invalid syntax を修正

--- achievement_notificator.py
+++ achievement_notificator.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
         total = 0
         for item in all_list:
             total += item[4]
-        total = {:,}'.format(total)
+        total = '{:,}'.format(total)
 
         message = "[info][title]【祝】新規成果発生のお知らせ！[/title]"
         message += f"新規で【{len(data)}件】成果が発生しました。\n"
