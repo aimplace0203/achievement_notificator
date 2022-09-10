ca6e6feb88b388617ea32b58569f0d59ca041cfa
kefujiwa <kefujiwa@student.42tokyo.jp>
2022-09-11 04:38:42 +0900

    feat: リファクタリング対応
    - チャップアップ成果通知処理にAFBとLINKAの成果も追加

--- achievement_notificator_bresmile.py
+++ achievement_notificator_bresmile.py
@@ -53,218 +53,6 @@ def readCsvData(csvPath, code):
             yield row
 
 
-# LinkA #
-def getDataFromLinkA(downloadsDirPath, d):
-    url = "https://link-ag.net/partner/sign_in"
-    login = os.environ['LINKA_ID']
-    password = os.environ['LINKA_PASS']
-
-    ua = UserAgent()
-    logger.debug(f'importCsvFromLinkA: UserAgent: {ua.chrome}')
-
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument(f'user-agent={ua.chrome}')
-
-    prefs = {
-        "profile.default_content_settings.popups": 1,
-        "download.default_directory": 
-                os.path.abspath(downloadsDirPath),
-        "directory_upgrade": True
-    }
-    options.add_experimental_option("prefs", prefs)
-
-    try:
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-
-        driver.get(url)
-        driver.maximize_window()
-        driver.implicitly_wait(30)
-
-        driver.find_element_by_id('login_id').send_keys(login)
-        driver.find_element_by_id('password').send_keys(password)
-        driver.find_element_by_xpath('//input[@type="submit"]').click()
-
-        logger.debug('importCsvFromLinkA: linka login')
-        driver.get('https://link-ag.net/partner/achievements')
-        driver.implicitly_wait(30)
-
-        driver.find_elements_by_id('occurrence_time_occurrence_time')[d].click()
-        driver.implicitly_wait(30)
-
-        logger.info('importCsvFromLinkA: select date range')
-        driver.implicitly_wait(30)
-
-        driver.find_element_by_xpath('//input[@value="検索"]').click()
-        driver.implicitly_wait(30)
-
-        dropdown = driver.find_element_by_id("separator")
-        select = Select(dropdown)
-        select.select_by_value('comma')
-        driver.implicitly_wait(30)
-
-        driver.find_element_by_class_name('partnerMain-btn.partnerMain-btn-md.partnerMain-btn').click()
-        logger.info('getDataFromLinkA: Complete download')
-        sleep(3)
-
-        driver.close()
-        driver.quit()
-    except Exception as err:
-        logger.debug(f'Error: importCsvFromLinkA: {err}')
-        exit(1)
-
-def getCsvPathLinkA(dirPath, d):
-    os.makedirs(dirPath, exist_ok=True)
-    #getDataFromLinkA(dirPath, d)
-
-    csvPath = getLatestDownloadedFileName(dirPath)
-    logger.info(f"achievement_notificator: download completed: {csvPath}")
-    return csvPath
-
-def getAchievementDataLinkA(data, prev):
-    header = data.pop(0)
-    for i, d in enumerate(header):
-        if re.search('発生日時', d):
-            date = int(i)
-        elif re.search('報酬金額（税込）', d):
-            reward = int(i)
-        elif re.search('広告名', d):
-            ad = int(i)
-    
-    diff = len(data) - len(prev)
-
-    while diff > 0:
-        item = data.pop(0)
-        yield [item[date], item[reward], item[ad]]
-        diff -= 1
-
-def createCsvFileLinkA(data, outputFilePath):
-    header = ["発生日時","報酬金額（税込）","広告名"]
-    with open(outputFilePath, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f, delimiter=',', lineterminator='\r\n',  quoting=csv.QUOTE_ALL)
-        writer.writerow(header)
-        writer.writerows(data)
-
-def getInfoLinkA():
-    previous = []
-    if os.path.exists(f'./data/chapup/linka.csv'):
-        data = list(readCsvData(f'./data/chapup/linka.csv', 'utf-8'))
-        if len(data) > 0:
-            data.pop(0)
-            previous = data
-    all_list = previous
-
-    csvPath = getCsvPathLinkA('./csv/linka/', 0)
-
-    data = list(readCsvData(csvPath, 'utf-16'))
-    new = list(getAchievementDataLinkA(data, previous))
-    new.reverse()
-
-    total_price = 0
-    for item in all_list:
-        total_price += int(int(item[1]) / 11 * 10)
-
-    message = ''
-    if len(new) == 0:
-        logger.info("LinkA: No new achievements")
-    else:
-        all_list.extend(new)
-        os.makedirs(f'./data/chapup/', exist_ok=True)
-        createCsvFileLinkA(all_list, f'./data/chapup/linka.csv')
-
-        for item in new:
-            total_price += int(int(item[1]) / 11 * 10)
-            message += '\n＋＋＋\n\n'
-            message += f'発生日：{item[0]}\n'
-            reward = '{:,}'.format(int(int(item[1]) / 11 * 10))
-            message += f'報酬：¥{reward}\n'
-            message += f'広告名：{item[2]}\n'
-    return {
-        'new': len(new),
-        'total': len(all_list),
-        'total_price': total_price,
-        'message': message
-    }
-
-
-# Afb #
-def getAchievementDataAfb(data, prev):
-    header = data.pop(0)
-    for i, d in enumerate(header):
-        if re.search('発生日', d):
-            date = int(i)
-        elif re.search('報酬', d):
-            reward = int(i)
-        elif re.search('サイト名', d):
-            site = int(i)
-        elif re.search('PID', d):
-            pid = int(i)
-        elif re.search('プロモーション名', d):
-            name = int(i)
-    
-    chapup_data = []
-    for item in data:
-        if re.search('育毛剤',item[site]) or re.search('チャップアップ',item[site]):
-            chapup_data.append(item)
-    
-    diff = len(chapup_data) - len(prev)
-
-    while diff > 0:
-        item = chapup_data.pop(0)
-        yield [item[date], item[reward], item[site], item[pid], item[name]]
-        diff -= 1
-
-def createCsvFileAfb(data, outputFilePath):
-    header = ["発生日","報酬","サイト名","PID","プロモーションID"]
-    with open(outputFilePath, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f, delimiter=',', lineterminator='\r\n',  quoting=csv.QUOTE_ALL)
-        writer.writerow(header)
-        writer.writerows(data)
-
-def getInfoAfb():
-    previous = []
-    if os.path.exists(f'./data/chapup/afb.csv'):
-        data = list(readCsvData(f'./data/chapup/afb.csv', 'utf-8'))
-        if len(data) > 0:
-            data.pop(0)
-            previous = data
-    all_list = previous
-
-    afbCsvPath = getLatestDownloadedFileName('./csv/afb/')
-
-    data = list(readCsvData(afbCsvPath, 'cp932'))
-    new = list(getAchievementDataAfb(data, previous))
-    new.reverse()
-
-    total_price = 0
-    for item in all_list:
-        total_price += int(item[1])
-
-    message = ""
-    if len(new) == 0:
-        logger.info("Afb: No new achievements")
-    else:
-        all_list.extend(new)
-        os.makedirs(f'./data/chapup/', exist_ok=True)
-        createCsvFileAfb(all_list, f'./data/chapup/afb.csv')
-
-        for item in new:
-            total_price += int(item[1])
-            message += '\n＋＋＋\n\n'
-            message += f'発生日：{item[0]}\n'
-            reward = '{:,}'.format(int(item[1]))
-            message += f'報酬：¥{reward}\n'
-            message += f'サイト名：{item[2]}\n'
-            message += f'プロモーションID：{item[3]}\n'
-            message += f'プロモーション名：{item[4]}\n'
-    return {
-        'new': len(new),
-        'total': len(all_list),
-        'total_price': total_price,
-        'message': message
-    }
-
-
 # Media #
 def importCsvFromMedia(downloadsDirPath, uname, domain):
     login = os.environ[f'{uname}_ID']
@@ -427,8 +215,8 @@ if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         if sys.argv[1] == 'cleanup':
-            shutil.rmtree('./csv/chapup/')
-            shutil.rmtree('./data/chapup/')
+            shutil.rmtree('./csv/bresmile/')
+            shutil.rmtree('./data/bresmile/')
             exit(0)
 
     new = 0
@@ -439,19 +227,7 @@ if __name__ == '__main__':
     code = dict()
     output = dict()
     try:
-        res = getInfoLinkA()
-        new += res['new']
-        total += res['total']
-        total_price += res['total_price']
-        body_message += res['message']
-
-        res = getInfoAfb()
-        new += res['new']
-        total += res['total']
-        total_price += res['total_price']
-        body_message += res['message']
-
-        res = getInfoMedia('chapup', 'CHAPUP', 'chapup.jp', 22000)
+        res = getInfoMedia('bresmile', 'BRESMILE', 'bresmile.jp', 10000)
         new += res['new']
         total += res['total']
         total_price += res['total_price']
@@ -468,7 +244,7 @@ if __name__ == '__main__':
             message += '[/info]'
 
             #print(message)
-            sendChatworkNotification(message, 'CHAPUP')
+            sendChatworkNotification(message, 'BRESMILE')
 
         logger.info("achievement_notificator: Finish")
         exit(0)
